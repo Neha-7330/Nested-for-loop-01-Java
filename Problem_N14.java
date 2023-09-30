efe7710a7e2536386844634ba5d48087afd57cd4
Neha Pudake <126329167+Neha-7330@users.noreply.github.com>
2023-09-30 21:44:04 +0530

    Add files via upload

---
+++ Problem_N14.java
@@ -0,0 +1,17 @@
+/* 
+    1A 1A 1A 
+    1A 1A 1A
+    1A 1A 1A
+*/
+
+public class Problem_N14 {
+    public static void main(String[] args){
+
+        for(int i = 1; i <= 3; i++){
+            for(int j =1; j <= 3; j++){
+                System.out.print("1A  ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Problem_N18.java
@@ -0,0 +1,22 @@
+/* 
+    d  d  d  d 
+    c  c  c  c
+    b  b  b  b
+    a  a  a  a
+*/
+
+public class Problem_N18 {
+
+    public static void main(String[] args){
+
+        char ch = 'd' ;
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 4; j++){
+                System.out.print(ch + "  ");
+            }
+            ch--;
+            System.out.println(" ");
+        }
+
+    }
+}
